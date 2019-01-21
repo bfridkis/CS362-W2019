423f9c23ea0e19ec0fb5cd8985ade727d093da92
Benjamin Fridkis <fridkisb@flip1.engr.oregonstate.edu>
2019-01-21 09:39:45 -0800

    Refactored so Adventurer, Smithy, Cutpurse, Minion, and Ambassador are handled by seperately defined functions (per assignment 2)

---
+++ projects/fridkisb/dominion/cardEffects.h
@@ -0,0 +1,14 @@
+#ifndef _CARDEFFECTS_H
+#define _CARDEFFECTS_H
+
+#include "dominion.h"
+
+int adventurerEffect(int currentPlayer, struct gameState *state);
+int smithyEffect(int currentPlayer, struct gameState *state, int handPos);
+int cutpurseEffect(int currentPlayer, struct gameState *state, int handPos);
+int minionEffect(int currentPlayer, struct gameState *state, int choice1,
+	int choice2, int handPos);
+int ambassadorEffect(int currentPlayer, struct gameState *state, int choice1,
+	int choice2, int handPos);
+
+#endif

65af2432135b8380e6190605e23351bc19c2165e
Benjamin Fridkis <bfridkis@gmail.com>
2019-01-27 08:35:59 -0800

    Added additional code for unittest2

--- projects/fridkisb/dominion/_unittest2helper.c
+++ projects/fridkisb/dominion/_unittest2helper.c
@@ -50,9 +50,8 @@ int _unittest2helper(int testCase, int k[], struct gameState* G,
 		//Treasure_map
 		case(i == 0):
 			returnValue = gainCard(26, G, 0, 0);
-			
-void _unittest2helper2(int rv, int expectedRV, struct gameState* G,
-	int expectedPile, int expectedIdx, int cardValue, int k[]){
+	
+void _unittest2helper2(unittest2helper2struct ut2h2s){
 
 	//Check to make sure return value is as expected (0)
 	if(rv != expectedRV){
@@ -67,7 +66,7 @@ void _unittest2helper2(int rv, int expectedRV, struct gameState* G,
 	}
 	
 	//Check to make sure card was gained to expected pile
-	if(expectedPile = 0 && G->discard[0][expectedIdx] != cardValue){
+	if(expectedPile == 0 && G->discard[0][expectedIdx] != cardValue){
 		if(++(*failedTestCount) <= MAX_FAILS){
 		  failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
 		  sprintf(failures[(*failedTestCount)-1].description,
@@ -77,7 +76,7 @@ void _unittest2helper2(int rv, int expectedRV, struct gameState* G,
 		}
 	  failCt++;	
 	}
-	else if(expectedPile = 1 && G->deck[0][expectedIdx] != cardValue){
+	else if(expectedPile == 1 && G->deck[0][expectedIdx] != cardValue){
 		if(++(*failedTestCount) <= MAX_FAILS){
 		  failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
 		  sprintf(failures[(*failedTestCount)-1].description,
@@ -87,7 +86,7 @@ void _unittest2helper2(int rv, int expectedRV, struct gameState* G,
 		}
 	  failCt++;	
 	}
-	else if(expectedPile = 2 && G->hand[0][expectedIdx] != cardValue){
+	else if(expectedPile == 2 && G->hand[0][expectedIdx] != cardValue){
 		if(++(*failedTestCount) <= MAX_FAILS){
 		  failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
 		  sprintf(failures[(*failedTestCount)-1].description,
@@ -99,9 +98,86 @@ void _unittest2helper2(int rv, int expectedRV, struct gameState* G,
 	}
 	
 	//Check to make sure no cards are in unexpected locations
-	for(j = 0; j < MAX_DECK; j++){			
-		if(j < expectedIdx && G->discount[0][j] != k[j] ||
-		   j > expectedIdx && G->discount[0][j] != -1){
+	for(j = 0; (expectedPile == 0 || expectedPile == 1) && 
+			j < MAX_DECK; j++){			
+		if(expectedPile = 0 && 
+		   ((j <= expectedIdx && G->discard[0][j] != k[j]) ||
+		    (j > expectedIdx && G->discard[0][j] != -1))){
+			failCt++;
+			if(++(*failedTestCount) <= MAX_FAILS && j <= expectedIdx){
+				failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
+				sprintf(failures[(*failedTestCount)-1].description,
+				"Card not gained to proper location or additional card gained.\n"
+				"Expected card %d at idx %d of discard pile ; Observed %d",
+				k[j], expectedIdx, G->discard[0][j]);
+				break;
+			}
+			else if(++(*failedTestCount) <= MAX_FAILS && j > expectedIdx){
+				failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
+				sprintf(failures[(*failedTestCount)-1].description,
+				"Card not gained to proper location or additional card gained.\n"
+				"Expected -1 at idx %d of discard pile ; Observed %d",
+				expectedIdx, G->discard[0][j]);
+				break;
+			}
+		}
+		else if(expectedPile = 1 && 
+		   ((j <= expectedIdx && G->deck[0][j] != k[j]) ||
+		    (j > expectedIdx && G->deck[0][j] != -1))){
+			failCt++;
+			if(++(*failedTestCount) <= MAX_FAILS && j <= expectedIdx){
+				failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
+				sprintf(failures[(*failedTestCount)-1].description,
+				"Card not gained to proper location or additional card gained.\n"
+				"Expected card %d at idx %d of deck ; Observed %d",
+				k[j], expectedIdx, G->deck[0][j]);
+				break;
+			}
+			else if(++(*failedTestCount) <= MAX_FAILS && j > expectedIdx){
+				failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
+				sprintf(failures[(*failedTestCount)-1].description,
+				"Card not gained to proper location or additional card gained.\n"
+				"Expected -1 at idx %d of deck pile ; Observed %d",
+				expectedIdx, G->deck[0][j]);
+				break;
+			}
+		}
+	}
+	for(j = 0; expectedPile == 2 && j < MAX_HAND; j++){	
+		if((j <= expectedIdx && G->hand[0][j] != k[j]) ||
+		    (j > expectedIdx && G->hand[0][j] != -1)){
+			failCt++;
+			if(++(*failedTestCount) <= MAX_FAILS && j <= expectedIdx){
+				failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
+				sprintf(failures[(*failedTestCount)-1].description,
+				"Card not gained to proper location or additional card gained.\n"
+				"Expected card %d at idx %d of hand ; Observed %d",
+				k[j], expectedIdx, G->hand[0][j]);
+				break;
+			}
+			else if(++(*failedTestCount) <= MAX_FAILS && j > expectedIdx){
+				failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
+				sprintf(failures[(*failedTestCount)-1].description,
+				"Card not gained to proper location or additional card gained.\n"
+				"Expected -1 at idx %d of hand ; Observed %d",
+				expectedIdx, G->hand[0][j]);
+				break;
+			}
+		}
+	}
+	
+	//Check to make sure all pile counts are as expected
+	if(G->discardCount[0] != expectedIdx + 1 && 
+		++(*failedTestCount) <= MAX_FAILS){
+		failures[(*failedTestCount)-1].lineNumber = BUYCARDCALL1;
+		sprintf(failures[(*failedTestCount)-1].description,
+		"Discard pile count updated incorrectly.\n"
+		"Expected %d ; Observed %d", expectedIdx + 1, G->discountCount[0]);
+	}
+	
+	//***Deck Count Check here...
+	
+	//***Hand Count Check here...
 			
 				
 	
--- projects/fridkisb/dominion/_unittest2helper.h
+++ projects/fridkisb/dominion/_unittest2helper.h
@@ -25,7 +25,23 @@ struct failedTest {
 
 typedef struct failedTest failedTest;
 
+struct unittest2helper2struct {
+	int rv;
+	int expectedRV;
+	struct gameState* G;
+	int expectedPile;
+	int expectedIdx;
+	int cardValue; 
+	int* k;
+	failedTest failures;
+	int* failCt;
+};
+
+typedef struct unittest2helper2struct unittest2helper2struct;
+
 int _unittest2helper(int testCase, int k[], struct gameState* G,
 	failedTest failures[], int* failedTestCount);
+	
+void _unittest2helper2(unittest2helper2struct ut2h2s);
 
 #endif
